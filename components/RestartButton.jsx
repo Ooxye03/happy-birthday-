affda9e75455a4b10553068ee201a9d1a6037494
Ooxye03 <ricoerfan29@gmail.com>
2025-07-18 11:49:43 +0700

    Update RestartButton.jsx

--- components/RestartButton.jsx
+++ components/RestartButton.jsx
@@ -2,7 +2,7 @@ export default function RestartButton({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="mt-4 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded transition"
+      className="mt-6 px-4 py-2 bg-red-500 hover:bg-red-600 text-white text-sm rounded-lg transition-all shadow-sm"
     >
       🔁 Ulangi dari awal
     </button>
